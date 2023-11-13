41b70784010cd1dbb4fefe7533112c9ad214a6c9
Eliud Githuku <githukueliud@gmail.com>
2023-11-13 15:36:53 +0300

    homescreen refactor

--- app/src/main/java/com/example/eagermarketplace/presentation/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/eagermarketplace/presentation/homescreen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.eagermarketplace.presentation.homescreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,23 +8,40 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.eagermarketplace.R
 import com.example.eagermarketplace.presentation.homescreen.components.CategoriesCard
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
 
 ) {
+    var searchQuery by remember { mutableStateOf("") }
 //    val categoryItems = listOf(
 //        CategoriesCard(
 //            categoryImage = R.drawable.soko_5,
@@ -43,9 +61,35 @@ fun HomeScreen(
 //        )
 //    )
     Column(
-        modifier  = Modifier.fillMaxSize(),
+        modifier  = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp, start = 10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.soko_5),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(6.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            OutlinedTextField(
+                value = searchQuery,
+                onValueChange = {searchQuery = it},
+                trailingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null)},
+                label = { Text(text = "Search products") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
@@ -61,9 +105,10 @@ fun HomeScreen(
             Spacer(modifier = Modifier.width(10.dp))
             CategoriesCard(
                 categoryImage = R.drawable.delivery,
-                categoryName = "Gas Delivery"
+                categoryName = "Delivery"
             )
         }
+        Spacer(modifier = Modifier.height(8.dp))
     }
 }
 
--- app/src/main/java/com/example/eagermarketplace/presentation/homescreen/components/CategoriesCard.kt
+++ app/src/main/java/com/example/eagermarketplace/presentation/homescreen/components/CategoriesCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -34,8 +35,8 @@ fun CategoriesCard(
 ) {
     Card(
         modifier = Modifier
-            .height(80.dp)
-            .width(80.dp)
+            .height(100.dp)
+            .width(100.dp)
     ) {
         Column(
             modifier = Modifier.fillMaxSize()
@@ -45,7 +46,7 @@ fun CategoriesCard(
             Image(
                 painter = painterResource(id = categoryImage),
                 contentDescription = null,
-                modifier = Modifier.size(50.dp)
+                modifier = Modifier.height(70.dp).fillMaxWidth()
                     .clip(RoundedCornerShape(5.dp)),
                 contentScale = ContentScale.Crop
             )
